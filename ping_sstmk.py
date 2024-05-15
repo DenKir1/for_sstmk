73531f7d9b20741d446027685502734a00877f62
Den1 <kird.den@rambler.ru>
2024-05-15 22:05:18 +0300

    fix ping response

--- ping_sstmk.py
+++ ping_sstmk.py
@@ -7,7 +7,8 @@ import os
 address = "sstmk.ru"
 
 response = os.popen(f"ping {address} ").read()
-if response:
-    print(f"IP-address of {address} is  - {response[28:41]} - Ping Successful, Host is UP!")
-else:
+
+if ("Request timed out." or "unreachable") in response:
     print(f"Ping Unsuccessful, Host is DOWN.")
+else:
+    print(f"IP-address of {address} is  - {response[28:41]} - Ping Successful, Host is UP!")
